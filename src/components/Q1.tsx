a0d025133c52e24024e4cae7d22ad2cbeea5c10f
briankuei <vx3012564897@gmail.com>
2023-01-22 10:18:29 +0000

    Update Q1.tsx and Q2.tsx

--- src/components/Q1.tsx
+++ src/components/Q1.tsx
@@ -1,41 +1,46 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useState } from "react";
 
 const Card = ({ index }: { index: number }) => {
-  let color = 'bg-pink-100';
+  let color = "bg-pink-100";
   if (!((index + 1) % 3)) {
-    color = 'bg-red-500'
+    color = "bg-red-500";
   }
   if (!((index + 1) % 5)) {
-    color = 'bg-black'
+    color = "bg-black";
   }
-  return (
-    <div className={`w-[140px] h-[200px] rounded-lg ${color}`}></div>
-  )
-}
+  return <div className={`w-[140px] h-[200px] rounded-lg ${color}`}></div>;
+};
 
 function Q1() {
-  const [count, setCount] = useState('0')
+  const [count, setCount] = useState("0");
 
   const onChange = (e: FormEvent<HTMLInputElement>): void => {
     const pattern = /^\d+$/;
-    if(pattern.test(e.currentTarget.value)) {
+    if (pattern.test(e.currentTarget.value)) {
       setCount(e.currentTarget.value);
     } else {
-      setCount('0');
+      setCount("0");
     }
   };
 
   return (
     <div className="h-full flex flex-col justify-start items-center">
       <h1 className="text-5xl">第一題</h1>
-      <input type="number" className="my-5 bg-gray-100" onChange={onChange} placeholder="輸入正整數"/>
-      <div className="w-[300px] rounded-2xl grid grid-cols-2 gap-[10px] justify-items-center">
-        {
-          Array(+count ?? 0).fill(0).map((_, i) => <Card key={i} index={i} />)
-        }
+      <input
+        className="my-5 bg-gray-100"
+        onChange={onChange}
+        type="number"
+        placeholder="輸入正整數"
+      />
+      <div className="w-[300px] rounded-2xl grid grid-cols-2 gap-[10px] justify-items-center bg-gray-300">
+        {Array(+count ?? 0)
+          .fill(0)
+          .map((_, i) => (
+            <Card key={i} index={i} />
+          ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Q1
+export default Q1;
--- src/components/Q2.tsx
+++ src/components/Q2.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import useInterval from '../hooks/useInterval';
-import usePrevious from '../hooks/usePrevious';
+import { useState } from "react";
+import useInterval from "../hooks/useInterval";
+import usePrevious from "../hooks/usePrevious";
 
 const DATA = { price: 0, order: 0 };
 
@@ -12,24 +12,24 @@ function Q2() {
     setData({
       price: Math.floor(Math.random() * 201) - 100,
       order: Math.floor(Math.random() * 201) - 100,
-    })
-  }, 1000)
+    });
+  }, 1000);
 
   const changeTextColor = (key: keyof typeof DATA) => {
     const preData = previousData?.[key] ?? 0;
     const curData = data[key];
-    return curData > preData ? 'animate-text-green' : 'animate-text-red';
-  }
+    return curData > preData ? "animate-text-green" : "animate-text-red";
+  };
 
   return (
     <div className="h-full flex flex-col justify-start items-center">
       <h1 className="text-5xl">第二題</h1>
-      <div className='w-[200px] flex flex-row justify-center space-x-5 my-5 text-black'>
-        <p className={changeTextColor('price')}>price: {data.price}</p>
-        <p className={changeTextColor('order')}>order: {data.order}</p>
+      <div className="w-[200px] flex flex-row justify-center space-x-5 my-5 text-black">
+        <p className={changeTextColor("price")}>price: {data.price}</p>
+        <p className={changeTextColor("order")}>order: {data.order}</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Q2
+export default Q2;
